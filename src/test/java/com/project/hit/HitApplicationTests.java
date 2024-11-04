a75418294ffc0eb201e327c610788a68928c1625
hirit98 <navey777@gmail.com>
2024-11-04 14:37:05 +0900

    first commit

---
+++ src/test/java/com/project/hit/HitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
