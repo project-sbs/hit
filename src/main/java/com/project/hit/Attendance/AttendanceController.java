854c3bae4343d33d7fa7428919776021a39fd681
hirit98 <navey777@gmail.com>
2024-11-22 13:28:44 +0900

    호성 - 어노테이션 통일, 안쓰는 코드 삭제, db 패스워드 수정

--- src/main/java/com/project/hit/Attendance/AttendanceController.java
+++ src/main/java/com/project/hit/Attendance/AttendanceController.java
@@ -1,37 +1,21 @@
 package com.project.hit.Attendance;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.security.Principal;
-import java.util.Collection;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class AttendanceController {
 
-    @Autowired
-    private AttendanceService attendanceService;
+    private final AttendanceService attendanceService;
 
     @GetMapping("/Attendance/AttendanceCalendar")
-    public String showAttendanceCalendar(Authentication authentication, Model model) {
-        boolean isProfessor = false;
-
-        // 사용자 권한 확인
-        if (authentication != null) {
-            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-            isProfessor = authorities.stream()
-                    .anyMatch(auth -> auth.getAuthority().equals("ROLE_교수"));
-        }
-
-        model.addAttribute("isProfessor", isProfessor); // 뷰에서 사용
+    public String showAttendanceCalendar() {
         return "Attendance/AttendanceCalendar";
     }
 
