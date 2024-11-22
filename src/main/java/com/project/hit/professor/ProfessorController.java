72c47e251be7c0c23b1d0a3f0225f8f55e78047b
hirit98 <navey777@gmail.com>
2024-11-22 14:05:49 +0900

    호성 - 교수 home, report, score, info 페이지 추가, 교수 로그인 오류 해결

--- src/main/java/com/project/hit/professor/ProfessorController.java
+++ src/main/java/com/project/hit/professor/ProfessorController.java
@@ -10,7 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/p")
 public class ProfessorController {
 
-    /*@GetMapping("/home")
-    public String home() {return "professor/professorHome";}*/
+    @GetMapping("/home")
+    public String home() {return "portal/professor/professor_home";}
+
+    @GetMapping("/info")
+    public String info() {return "portal/professor/professor_info";}
+
+    @GetMapping("/report")
+    public String report() {return "portal/professor/professor_report_check";}
+
+    @GetMapping("/score")
+    public String score() {return "portal/professor/professor_score";}
 
 }
